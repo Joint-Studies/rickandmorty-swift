f419175b7093577648a4067959fe0f4f147c80fd
Crewlan <rodrigoa.work@gmail.com>
2024-09-19 22:08:29 -0300

    create project

---
+++ rickandmorty-swift/rickandmorty-swift/rickandmorty_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  rickandmorty_swiftApp.swift
+//  rickandmorty-swift
+//
+//  Created by Rodrigo Silva on 19/09/24.
+//
+
+import SwiftUI
+
+@main
+struct rickandmorty_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
